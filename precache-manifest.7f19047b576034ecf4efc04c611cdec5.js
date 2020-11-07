940591c3bafa69017123fb7225fa6709836cefea
Lukas Viskanta <lukasviskanta@gmail.com>
2020-11-06 22:31:20 -0800

    Updates

---
+++ precache-manifest.7f19047b576034ecf4efc04c611cdec5.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6328095de39fcf478f5bfdfb48224171",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "00e0a2363bb8671841f5",
+    "url": "/project/static/css/2.3b56cc47.chunk.css"
+  },
+  {
+    "revision": "c79c6ede18f9172c5618",
+    "url": "/project/static/css/main.c4bc5652.chunk.css"
+  },
+  {
+    "revision": "00e0a2363bb8671841f5",
+    "url": "/project/static/js/2.0b21445b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project/static/js/2.0b21445b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79c6ede18f9172c5618",
+    "url": "/project/static/js/main.e32cf4f7.chunk.js"
+  },
+  {
+    "revision": "7a7628528d267cbc3ebd",
+    "url": "/project/static/js/runtime-main.15d782a1.js"
+  },
+  {
+    "revision": "bacc5c9ffbda0d0f127e78a685f4752c",
+    "url": "/project/static/media/airforce.bacc5c9f.jpg"
+  },
+  {
+    "revision": "4870f1f485863e3ddb2a821045d98d35",
+    "url": "/project/static/media/backup.4870f1f4.jpg"
+  },
+  {
+    "revision": "9e849cd70e48b4eea784ee02d07407cf",
+    "url": "/project/static/media/captcha.9e849cd7.jpg"
+  },
+  {
+    "revision": "74497f21b694b776530c724fc6e693a1",
+    "url": "/project/static/media/colosseum.74497f21.jpg"
+  },
+  {
+    "revision": "09feeb3fa2f4fc1bc1e8d2ba07f73e78",
+    "url": "/project/static/media/crunch.09feeb3f.jpg"
+  },
+  {
+    "revision": "8e3765a1e4dafd8f0a9b42041f5f7797",
+    "url": "/project/static/media/david.8e3765a1.jpg"
+  },
+  {
+    "revision": "90384136302b4e77ae0cfb6ff5b34599",
+    "url": "/project/static/media/designs_title.90384136.png"
+  },
+  {
+    "revision": "f6e7a44ade5d9ebca6461ae8f5ba2ae8",
+    "url": "/project/static/media/destroyer-01.f6e7a44a.jpg"
+  },
+  {
+    "revision": "95b2d8638648cefe5ded6bc95e2081c0",
+    "url": "/project/static/media/glue.95b2d863.jpg"
+  },
+  {
+    "revision": "655b06f09561406782812d25e517f32d",
+    "url": "/project/static/media/greece-02.655b06f0.jpg"
+  },
+  {
+    "revision": "3c46536d807fc0da680377d93e28e5a1",
+    "url": "/project/static/media/koolaid.3c46536d.jpg"
+  },
+  {
+    "revision": "17d62bea6b9f1fb233d260b025ccae89",
+    "url": "/project/static/media/portra.17d62bea.jpg"
+  },
+  {
+    "revision": "147fedac1b92ccd2e39aa1a79c1fb80f",
+    "url": "/project/static/media/rat.147fedac.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/project/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/project/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/project/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "a8b00df43d80684ebf14828dcc11393e",
+    "url": "/project/static/media/rex.a8b00df4.jpg"
+  },
+  {
+    "revision": "5559dedf71bf12ada6c6a8904b2476e2",
+    "url": "/project/static/media/self-portrait.5559dedf.svg"
+  },
+  {
+    "revision": "292c0161de156ccd6d8176e0547b0671",
+    "url": "/project/static/media/statue.292c0161.jpg"
+  },
+  {
+    "revision": "1c79ad484532f80fc7de70546caa5793",
+    "url": "/project/static/media/thanks_for.1c79ad48.svg"
+  },
+  {
+    "revision": "3350b9e2e1fefc2f47be690291202ffe",
+    "url": "/project/static/media/tiger.3350b9e2.jpg"
+  },
+  {
+    "revision": "3686c780edc8e4a990cf599c4a49d933",
+    "url": "/project/static/media/woman.3686c780.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project/precache-manifest.0c6a71194dd68e1408b646a5890faaca.js"
+  "/project/precache-manifest.7f19047b576034ecf4efc04c611cdec5.js"
 );
 
 self.addEventListener('message', (event) => {
